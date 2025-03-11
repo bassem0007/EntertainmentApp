e618d4c17f5a520c49895e16d0e04d81f29ab1cc
Bassem Riachi <bassem.riachy@gmail.com>
2025-03-11 16:03:57 +0100

    First commit

---
+++ src/main/java/plural/capstone2/EntertainmentApp/EntertainmentAppApplication.java
@@ -0,0 +1,13 @@
+package plural.capstone2.EntertainmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plural/capstone2/EntertainmentApp/EntertainmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package plural.capstone2.EntertainmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

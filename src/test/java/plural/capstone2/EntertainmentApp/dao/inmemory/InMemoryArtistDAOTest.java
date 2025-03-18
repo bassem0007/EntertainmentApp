73a48c5e50d33c84b3dfa1e4fb326dcf6834a37e
Bassem Riachi <bassem.riachy@gmail.com>
2025-03-18 15:49:36 +0100

    Added InMemoryTrackDAOTest.java

--- src/test/java/plural/capstone2/EntertainmentApp/dao/inmemory/InMemoryArtistDAOTest.java
+++ src/test/java/plural/capstone2/EntertainmentApp/dao/inmemory/InMemoryArtistDAOTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class InMemoryArtistDAOTest {
+public class InMemoryArtistDAOTest {
 
     private Artist artist1;
     private Artist artist2;
